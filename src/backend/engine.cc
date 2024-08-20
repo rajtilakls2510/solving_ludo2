ce54604ef518a5a15ae80f1ef0d3477654291bc7
Rajtilak Pal <rajtilakls2510@gmail.com>
2024-08-20 20:03:21 +0530

    1. Added State methods 2. Impl State reset

--- src/backend/engine.cc
+++ src/backend/engine.cc
@@ -3,6 +3,7 @@
 
 #include "engine.hpp"
 
+
 // ========================= GameConfig methods ====================================
 
 GameConfig::GameConfig(std::vector<std::vector<std::string>> player_colour_choices) {
@@ -80,11 +81,118 @@ GameConfig::~GameConfig() {
     delete[] this->player_colours;
 }
 
+// ========================= State methods ====================================
+
+State::State(short n_players) {
+    this->n_players = n_players;
+    game_over = false;
+    current_player = 0;
+    num_more_moves = 0;
+    dice_roll = 0;
+    last_move_id = 0;
+    player_pos_pawn = new int[n_players * 93]();
+    num_blocks = 0;
+    // Note: 'blocks' will be initialized automatically
+    previous_pawn_pos = 0;
+}
+
+StatePtr State::copy() {
+    StatePtr new_state = std::make_shared<State>(n_players);
+    new_state->game_over = this->game_over;
+    new_state->current_player = this->current_player;
+    new_state->num_more_moves = this->num_more_moves;
+    new_state->dice_roll = this->dice_roll;
+    new_state->last_move_id = this->last_move_id;
+    std::memcpy(new_state->player_pos_pawn, this->player_pos_pawn, n_players * 93 * sizeof(int));
+    new_state->num_blocks = this->num_blocks;
+    for (short block_idx = 0; block_idx < new_state->num_blocks; block_idx++)
+        new_state->blocks[block_idx] = this->blocks[block_idx];
+    new_state->previous_pawn_pos = this->previous_pawn_pos;
+    return new_state;
+}
+
+std::string State::repr() {
+    
+    // Creating map of names
+    std::string pawn_name[] = {"", "R1", "R2", "R3", "R4", "G1", "G2", "G3", "G4", "Y1", "Y2", "Y3", "Y4", "B1", "B2", "B3",
+                     "B4"};
+    std::vector<std::string> pos_name = {"", "RB1", "RB2", "RB3", "RB4", "GB1", "GB2", "GB3", "GB4", "YB1", "YB2", "YB3", "YB4", "BB1",
+                    "BB2", "BB3", "BB4"};
+    for (int i = 0; i < 52; i++)
+        pos_name.push_back("P"+(i+1));
+    std::vector<std::string> last_list = {
+        "RH1", "RH2", "RH3", "RH4", "RH5", "RH6", "GH1", "GH2", "GH3", "GH4", "GH5", "GH6",
+        "YH1", "YH2", "YH3", "YH4", "YH5", "YH6", "BH1", "BH2", "BH3", "BH4", "BH5", "BH6"
+    };
+    pos_name.insert(pos_name.end(), last_list.begin(), last_list.end());
+
+
+    // Creating printable representation
+    std::stringstream ss;
+    ss << "State: {\n";
+
+    ss << "\tn_players : " << n_players << ", \n";
+    ss << "\tgame_over : " << std::boolalpha << game_over << ", \n";
+    ss << "\tcurrent_player : " << current_player << ", \n";
+    ss << "\tnum_more_moves : " << num_more_moves << ", \n";
+    ss << "\tdice_roll : " << dice_roll << ", \n";
+    ss << "\tlast_move_id : " << last_move_id << ", \n";
+    for (short player = 0; player < n_players; player++) {
+        ss << "\tPlayer " << player << " : {\n\t\t";
+
+        for (short pos = 1; pos < 93; pos++) {
+            int p = player_pos_pawn[player * 93 + pos];
+            while (p != 0) {
+                ss << pawn_name[p % 17] << " : " << pos_name[pos] << ", ";
+                p /= 17;
+            }
+        }
+        ss << "\n\t}, \n";
+    }
+    
+    // Blocks
+    ss << "\tblocks: [\n";
+    for (short block_idx = 0; block_idx < num_blocks; block_idx++) {
+        
+        ss << "\t\t{ ";
+        ss << "pawns : [ ";
+        short p = blocks[block_idx].pawns;
+        while (p != 0) {
+            ss << pawn_name[p% 17] << ", ";
+            p /= 17;
+        }
+        ss << "], ";
+        ss << "pos : " << pos_name[blocks[block_idx].pos] << ", rigid: " << std::boolalpha << blocks[block_idx].rigid;
+        ss << " },\n";
+    }
+    ss << "\t], \n";
+
+    int ppp = previous_pawn_pos;
+    ss << "\tprevious_pawn_pos : [ \n";
+    while (ppp != 0) {
+        int pos = ppp % 93;
+        ppp /= 93;
+        int pawn = pawn % 17;
+        ppp /= 17;
+
+        ss << "\t\t{ pawn : " << pawn_name[pawn] << " , pos : " << pos_name[pos] << " },\n";   
+    }
+    ss << "\t]\n";
+
+
+    ss << "}\n";
+    return ss.str();
+}
+
+State::~State() {
+    delete[] player_pos_pawn;
+}
 
 // ========================= LudoModel methods ====================================
 
 LudoModel::LudoModel(std::shared_ptr<GameConfig> config) {
     this->config = config;
+    this->throw_gen = std::make_shared<ThrowGenerator>();
     this->stars = new short[93]();
     this->final_pos = new short[93]();
     this->colour_bases = new short[5 * 4]();
@@ -155,11 +263,45 @@ LudoModel::LudoModel(std::shared_ptr<GameConfig> config) {
     
 }
 
+StatePtr LudoModel::get_initial_state() {
+    StatePtr state = std::make_shared<State>(config->n_players);
+    state->game_over = false;
+    state->current_player = 0;
+
+    int throw_ = throw_gen->get_throw();
+    state->dice_roll = throw_;
+    
+    state->num_more_moves = 0;
+    if (throw_ == 6)
+        state->num_more_moves++;
+    
+    state->last_move_id = 0;
+
+    for (short pawn = 1; pawn < 17; pawn++) {
+        short colour = (pawn - 1) / 4 + 1;
+        short player = config->colour_player[colour];
+        short pos = pawn;
+        state->player_pos_pawn[player * 93 + pos] = pawn;
+    }
+
+    state->num_blocks = 0;
+    state->previous_pawn_pos = 0;
+    
+    return state;
+}
+
 LudoModel::~LudoModel() {
     delete[] this->stars;
     delete[] this->final_pos;
     delete[] this->colour_bases;
     delete[] this->colour_tracks;
+}
+
+// ========================= Ludo methods ====================================
+
+void Ludo::reset() {
+    winner = -1;
+    state = model->get_initial_state();
 }
 
 
@@ -168,8 +310,10 @@ int main() {
         {"red", "yellow"}, 
         {"blue", "green"}
     };
-    LudoModel model(std::make_shared<GameConfig>(colours_config));
-    // std::cout << config.repr();
+    Ludo game(std::make_shared<GameConfig>(colours_config));
+    game.reset();
+    std::cout << game.state->repr();
+    std::cout << game.state->copy()->repr() ;
     return 0;
 }
 
--- src/backend/engine.hpp
+++ src/backend/engine.hpp
@@ -6,37 +6,49 @@
 #include <iostream>
 #include <algorithm>
 #include <sstream>
+#include <random>
+#include <cstring>
 
 class Block {
 public:
     int pawns;
     short pos;
     bool rigid;
+
+    Block() : pawns(0), pos(0), rigid(false) {};
+    Block(int pawns, short pos, bool rigid) : pawns(pawns), pos(pos), rigid(rigid) {};
+    ~Block() {};
+
 };
 
 class State {
 public:
-    short n_players;
-    bool game_over;
-    short current_player;
-    short num_more_moves;
-    short dice_roll;
-    short last_move_id;
-    int* pawn_pos;
-    short num_blocks;
-    Block blocks[16];
+    short n_players;    // Number of players
+    bool game_over;     // Whether the game is over or not
+    short current_player;   // Who is the current player
+    short num_more_moves;   // Number of moves left for the current player
+    short dice_roll;        // All the dice throws added up from when the turn began
+    short last_move_id;     // Move id of the last move taken
+    int* player_pos_pawn;   // For each player, for each position, the pawns that are present
+    short num_blocks;       // Number of blocks present in the tracks
+    Block blocks[16];       // blocks present in the tracks
+    int previous_pawn_pos;  // Stack to hold pawn and their previous position before they were moved. 
+    // (This is kept to return the pawns in their original positions when there are 3 dice 6's)
 
-    StatePtr copy();    // Copies the state
+    State(short n_players);
+    ~State();
+    
+    std::shared_ptr<State> copy();    // Copies the state
+    std::string repr();
 };
 
 using StatePtr = std::shared_ptr<State>;
 
 class Move {
 public:
-    short n_rolls;
-    int* pawns;
-    short* current_positions;
-    short* destinations;
+    int pawn;       // Pawn that is being moved
+    short current_position;     // Current position of the pawn
+    short destination;          // Destination of the pawn
 };
 
 using MovePtr = std::shared_ptr<Move>;
@@ -52,6 +64,19 @@ public:
     std::string repr();     // Returns a printable representation of the current configuration
 };
 
+class ThrowGenerator {
+public:
+    ThrowGenerator() : gen(std::random_device{}()) {}
+
+    int get_throw() {
+        std::uniform_int_distribution<> distrib(1,6);
+        return distrib(gen);
+    }
+
+private:
+    std::mt19937 gen;
+};
+
 class LudoModel {
 private:
     short* stars;   // Array that defines the positions of stars
@@ -59,12 +84,36 @@ private:
     short* colour_tracks;   // Sequence of positions (tracks) which pawns follow for each colour 
     short* colour_bases;    // Base positions for each colour
     std::shared_ptr<GameConfig> config;    // Config object of the game
+
 public:
+    std::shared_ptr<ThrowGenerator> throw_gen;
+
     LudoModel(std::shared_ptr<GameConfig> config);
     ~LudoModel();
 
     std::vector<StatePtr> generate_next_states(StatePtr state, MovePtr move);   // Returns all next states given current state and move
     std::vector<MovePtr> all_possible_moves(StatePtr state);    // Returns all possible moves for a given state 
+    StatePtr get_initial_state();       // Returns the initial state that the game starts in
+
+};
+
+using LudoModelPtr = std::shared_ptr<LudoModel>;
+
+class Ludo {
+public:
+    StatePtr state;     // Holds the current state
+    LudoModelPtr model;     // Holds the model
+    short winner{-1};       // Winner of the game when the game has finished (-1: no winner yet)
+
+    Ludo(std::shared_ptr<GameConfig> config) {
+        this->model = std::make_shared<LudoModel>(config);
+        state = model->get_initial_state();
+        winner = -1;
+    }
+
+    void reset();
+
+    ~Ludo () {};
 
 };
 
